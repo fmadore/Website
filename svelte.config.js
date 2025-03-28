3b7f35853c93ff31be493747d413204a4be19237
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-03-28 12:23:47 +0100

    Add SPA redirect solution for GitHub Pages direct URL access

--- svelte.config.js
+++ svelte.config.js
@@ -18,9 +18,19 @@ const config = {
 		paths: {
 			base: dev ? '' : '/Website'
 		},
+		appDir: 'app', // Ensures all generated JS is under this directory
 		// Make sure all pages are pre-rendered
 		prerender: {
-			entries: ['*']
+			entries: ['*'],
+			handleHttpError: ({ path, referrer, message }) => {
+				// If the error is a missing page, return a 404 status
+				if (path.startsWith('/Website/')) {
+					console.warn(`Ignoring ${path} - redirecting via 404.html`);
+					return;
+				}
+				// Otherwise, throw the error and stop the build
+				throw new Error(message);
+			}
 		}
 	},
 	extensions: ['.svelte', '.svx']
