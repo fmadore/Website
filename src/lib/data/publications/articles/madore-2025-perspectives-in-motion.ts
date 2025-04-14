cb1c649d1e81ea8bc516ec99141575e34f39abb9
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-04-14 11:46:14 +0200

    Add new publication entry for "Perspectives in Motion" by Frédérick Madore
    - Created a new TypeScript file for the publication, including details such as title, authors, date, journal, and abstract.
    - Added a corresponding image for the publication to the static images directory.

---
+++ src/lib/data/publications/articles/madore-2025-perspectives-in-motion.ts
@@ -0,0 +1,21 @@
+import type { Publication } from '$lib/types';
+
+export const madore2025PerspectivesInMotion: Publication = {
+    id: "madore-2025-perspectives-in-motion",
+    type: "article",
+    title: "Perspectives in Motion: Conceptual Fields from the Global South, Workshop, 2-3 December 2024, ZMO",
+    authors: ["Madore, Frédérick"],
+    date: "April 2025",
+    dateISO: "2025-04",
+    year: 2025,
+    journal: "ZMO Bulletin",
+    issue: "46",
+    pages: "7",
+    language: "English",
+    doi: "10.58144/20250401-000",
+    abstract: "The workshop organised by the Religion and Intellectual Culture unit at ZMO explored how non-Western epistemologies hallenge and enrich dominant paradigms in decolonial knowledge production. Opening remarks by Arpan Roy, Kai Kresse, and Abdoulaye Sounaye framed discussions of processes of conceptualisation in postcolonial contexts and the transformative potential of such alternative frameworks.",
+    tags: ["Workshop report", "Global South", "conceptual fields"],
+    url: "https://doi.org/10.58144/20250401-000",
+    image: "images/publications/zmo-bulletin-46.jpg",
+    project: "",
+};
