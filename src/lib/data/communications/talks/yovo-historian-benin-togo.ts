c30cdfdab19efc849ffac42b2cec2d32d04d9bb2
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-04-21 09:29:36 +0200

    Update image format to WEBP and add hero image for 'Yovo Historian in Benin and Togo' communication for enhanced visual appeal and performance.

--- src/lib/data/communications/talks/yovo-historian-benin-togo.ts
+++ src/lib/data/communications/talks/yovo-historian-benin-togo.ts
@@ -13,7 +13,11 @@ export const yovoHistorianBeninTogo: Communication = {
     type: "workshop",
     language: "English",
     tags: ["Fieldwork", "Benin", "Togo", "Research Methodology", "Pandemic"],
-    image: "images/communications/yovo-historian-benin-togo.jpg",
+    image: "images/communications/yovo-historian-benin-togo.webp",
+    heroImage: {
+        src: "images/communications/yovo-historian-benin-togo.jpg",
+        alt: "A 'Yovo' Historian in Benin and Togo: Conducting Fieldwork in Times of Pandemic and Fears of Jihadism Spreading",
+    },
     coordinates: {
         latitude: 51.4861519467266,
         longitude: 11.969523822453674
