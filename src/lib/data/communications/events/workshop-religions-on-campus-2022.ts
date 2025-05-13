81bf76d1742b4d90994d8cb3af5d777666edf869
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-05-13 08:00:24 +0200

    Add new workshop details for "Religions on Campus: Coexisting Traditions, Reformulating the Secular and Life Projects," including participants, abstract, and event information to enhance communication and documentation.

---
+++ src/lib/data/communications/events/workshop-religions-on-campus-2022.ts
@@ -0,0 +1,48 @@
+import type { Communication } from '$lib/types/communication';
+
+// Workshop: Religions on Campus: Coexisting Traditions, Reformulating the Secular and Life Projects
+export const workshopReligionsOnCampus2022: Communication = {
+    id: "workshop-religions-on-campus-2022",
+    title: "Workshop “Religions on Campus: Coexisting Traditions, Reformulating the Secular and Life Projects”",
+    authors: ["Abdoulaye Sounaye", "Frédérick Madore", "Vincent Favier", "Mahamadou Bello Adamou", "Rakiya El Matine"], 
+    date: "31 October - 4 November 2022",
+    dateISO: "2022-10-31",
+    year: 2022,
+    conference: "Religions on Campus: Coexisting Traditions, Reformulating the Secular and Life Projects",
+    location: "Laboratoire d'Etudes et de Recherches sur les Dynamiques Sociales et le Développement Local (LASDEL)",
+    country: "Niger",
+    type: "event",
+    language: "English",
+    abstract: "The history of the university in Africa - in both colonial and postcolonial contexts - is intricately linked to political projects and societal transformation processes. As such, this institution of higher education has not only served state-building initiatives; it has also nurtured aspirations of individuals and communities in search for better livelihood and wellbeing. This is particularly acute at the creation of most of the universities across the continent. While state centered on the university to devise development policies, visions of societal progress and public service, individuals and collectivities have also found in the institution an opportunity to build life projects, invest in social becoming and secure status. Thus, from its inception, and as an institution expected to train administrative cadres and offer public service, the university was shaped by political agendas and societal dynamics. However, while universities fed promises of independence and development, precisely because of the relevance of higher education to social life and becoming, their campuses offered to many activists, both students and instructors, not only their first experience of ideological and political training, but also a site of contestations of established social norms, political regimes and orders. These contestations and the activism they inspired usually necessitated ideological formation and the acquisition and deployment of social organization skills. This is the case in times of political repression, but also when state policies, as is the case in the 1990s, failed to keep their promises of public education and employment. Within this context, in more recent years, university campuses have also turned into major sites of religious encounter, entrepreneurship, activism and competition. The cultivation of religious identities - resulting in religion feeding activism and social organization – has become a major feature of campus life. A challenge to both the prevailing ideas of university and the organization of campus life, these developments offer a reformulation of the secular roots of higher education while repositioning the experience of being student in individual and social life projects. How do we make sense of these processes? What allows for such developments? What are the narratives, aspirations and hopes that drive the actors at the center of these dynamics? How are their activism affecting the expectations, experiences and governance of the university? How to characterize the experience of being student in that context? More broadly, how are the attempts to reshape the campus as a moral space, rethink its curriculum as an instrument of life project, and reappropriate the main institution of higher education relate to the calls to decolonize it? How are students reconciling the appeal of religion and the calls to reform the university as they train to become social actors? How does the resort to religious discourse, identities and practices relate to current decolonial and social becoming aspirations? What is the moral regime and order that emerges in that context? The papers to be presented in this workshop are expected to take on one or several of these questions, focusing on Islam, Christianity and Traditional African religions. Papers discussing Salafi and Pentecostal presence, encounter and coexistence on campus will be given priority.",
+    tags: ["Remoboko", "Religion", "Campus", "Secularism", "University"],
+    url: "https://remoboko.hypotheses.org/religions-on-campus-coexisting-traditions-reformulating-the-secular-and-life-projects",
+    image: "images/communications/workshop-religions-on-campus-2022.webp",
+    heroImage: {
+        src: "images/communications/workshop-religions-on-campus-2022-hero.webp",
+        alt: "Group photo of the participants of the workshop",
+    },
+    coordinates: {
+        latitude: 13.51766342197132,
+        longitude: 2.101838007060417
+    },
+    participants: [
+        { name: "Abdoulaye Sounaye", role: "Convenor and Speaker", affiliation: "Leibniz-Zentrum Moderner Orient" },
+        { name: "Frédérick Madore", role: "Convenor and Speaker", affiliation: "Leibniz-Zentrum Moderner Orient" },
+        { name: "Vincent Favier", role: "Convenor and Speaker", affiliation: "Leibniz-Zentrum Moderner Orient" },
+        { name: "Mahamadou Bello Adamou", role: "Convenor and Speaker", affiliation: "Université Abdou Moumouni, Niger" },
+        { name: "Thomas Veret", role: "Speaker", affiliation: "Université Paris 3 Sorbonne Nouvelle" },
+        { name: "Agnès Badou", role: "Discussant", affiliation: "LASDEL Parakou, Benin" },
+        { name: "Mounkaila Abdou", role: "Speaker", affiliation: "Université Abdou Moumouni, Niger" },
+        { name: "Abdoulbaki Djibo", role: "Speaker", affiliation: "Université Abdou Moumouni, Niger" },
+        { name: "Jean Pierre Olivier de Sardan", role: "Speaker", affiliation: "LASDEL, Niger" },
+        { name: "Amadou Issoufou", role: "Speaker", affiliation: "Université Abdou Moumouni, Niger" },
+        { name: "Muhammad Yakasai", role: "Speaker", affiliation: "Humboldt University, Germany" },
+        { name: "Nadir A. Nasidi", role: "Speaker", affiliation: "Ahmadu Bello University, Nigeria" },
+        { name: "Oumarou Moussa", role: "Speaker", affiliation: "Université Abdou Moumouni, Niger" },
+        { name: "Sekou Sala Timbely", role: "Speaker", affiliation: "Université de Ségou, Mali" },
+        { name: "Katrin Bromber", role: "Speaker", affiliation: "Leibniz-Zentrum Moderner Orient" }
+    ],
+    project: "Religious Activism on Campuses in Togo and Benin" 
+};
+
+export default workshopReligionsOnCampus2022; 
