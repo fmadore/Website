67728b1e4d8b2504fceefbc13fab6918e8603ec0
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-05-13 08:11:53 +0200

    Add abstract to "Three Questions with Frédérick Madore" blog post to provide a comprehensive overview of Dr. Madore's research and its implications for understanding contemporary religious trends.

--- src/lib/data/publications/blogposts/three-questions-madore-2022.ts
+++ src/lib/data/publications/blogposts/three-questions-madore-2022.ts
@@ -12,6 +12,7 @@ export const threeQuestionsToMadore: Publication = {
     language: "English",
     doi: "10.58079/tldm",
     tags: ["Remoboko", "Interview"],
+    abstract: "In this interview, Dr Frédérick Madore explains his involvement with the REMOBOKO project at ZMO and how it fits with his research on Islam and Muslim societies in francophone West Africa. He outlines his current project on Salafism and Pentecostalism on university campuses in Benin and Togo, exploring Christian-Muslim interactions, lived religion, its impact on secular academic environments, and the emergence of new religious elites. Madore also discusses the broader implications of this work for understanding contemporary religious trends and challenging simplistic narratives of radicalisation in the region.",
     url: "https://doi.org/10.58079/tldm",
     image: "images/publications/three-questions-madore-2022.webp",
     heroImage: {
