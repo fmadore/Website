ce73eeb9caa5d4402924809cec3554ab8a418c62
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-05-15 21:10:33 +0200

    Add multiple publications to hadj-cote-divoire-2018.ts, expanding the academic references with new entries from 2019 to 2025, enhancing the resource's comprehensiveness and relevance in the context of Côte d'Ivoire.

--- src/lib/data/publications/articles/hadj-cote-divoire-2018.ts
+++ src/lib/data/publications/articles/hadj-cote-divoire-2018.ts
@@ -31,6 +31,61 @@ export const hadjCoteDIvoire: Publication = {
             title: "Les « imams chocos » ou la mutation de la figure de l'autorité religieuse musulmane en Côte d'Ivoire contemporaine",
             source: "In Islam and Muslim Life in West Africa: Practices, Trajectories and Influences, edited by Abdoulaye Sounaye and André Chappatte, 125-50. Berlin: De Gruyter, 2022",
             url: "https://doi.org/10.1515/9783110733204-007"
+        },
+        {
+            authors: ["Aboubakar Ouattara"],
+            year: 2021,
+            title: "Paradigme de l'entrepreneur dans le management et la communication des entreprises religieuses en Côte d'Ivoire",
+            source: "Akofena 2, no. 6 (2021): 209-22",
+            url: "https://web.archive.org/web/20240527233926/https://www.revue-akofena.com/wp-content/uploads/2021/09/16-retourT06-56-Aboubakar-OUATTARA-pp.209-222.pdf"
+        },
+        {
+            authors: ["Cédric Jourde", "Marie Brossier", "Muriel Gomez-Perez"],
+            year: 2021,
+            title: "The Politics of the Hajj Organisation in Senegal: Security, Legitimation and Clientelism",
+            source: "The Journal of Modern African Studies 59, no. 1 (2021): 1-20",
+            url: "https://doi.org/10.1017/S0022278X20000622"
+        },
+        {
+            authors: ["Bourahima Diomandé"],
+            year: 2021,
+            title: "Les luttes de positionnement autour des mosquées en Côte d'Ivoire : guerres idéologiques, conflits de leadership et de contrôle des fonds",
+            source: "Afrique et Développement 46, no. 4 (2021): 97-119"
+        },
+        {
+            authors: ["Issouf Binaté", "Yaya Dao"],
+            year: 2019,
+            title: "Organisations islamiques nigérianes en Côte d'Ivoire : les trajectoires concurrentielles de NASFAT et NAMFAT",
+            source: "Anthropologie & développement, no. 50 (2019): 87-105",
+            url: "https://doi.org/10.4000/anthropodev.831"
+        },
+        {
+            authors: ["Cédric Jourde", "Muriel Gomez-Perez", "Marie Brossier"],
+            year: 2025,
+            title: "The Political Management of the Hajj in Contemporary Times: An Introduction",
+            source: "In The Politics of the Hajj from a Comparative Perspective: States, Entrepreneurs, and Pilgrims, edited by Muriel Gomez-Perez, Cédric Jourde, and Marie Brossier, 1-25. Cham: Palgrave Macmillan, 2025",
+            url: "https://doi.org/10.1007/978-3-031-65349-0_1"
+        },
+        {
+            authors: ["Kouamé Félix Grodji", "Bony Guiblehon"],
+            year: 2024,
+            title: "Engager les religieux et les chefs traditionnels permet-il de réconcilier après une crise sociopolitique violente? Une réflexion à partir de l'expérience ivoirienne",
+            source: "Religioni e società XXXIX, no. 108 (2024): 38-47",
+            url: "https://doi.org/10.19272/202431301005"
+        },
+        {
+            authors: ["Christian Gérard Angue"],
+            year: 2022,
+            title: "Le pluralisme religieux sous le prisme du régime juridique de la laïcité dans les États d'Afrique noire francophone : Cameroun, Côte d'Ivoire, Gabon et Sénégal",
+            source: "Revue du droit des religions, no. 13 (2022): 173-89",
+            url: "https://doi.org/10.4000/rdr.1804"
+        },
+        {
+            authors: ["Gianni Dore"],
+            year: 2021,
+            title: "Capi locali e colonialismo in Eritrea : biografie di un potere subordinato (1937-1941)",
+            source: "Rome: Viella, 2021",
+            url: "http://digital.casalini.it/9788833136806"
         }
     ]
 }; 
