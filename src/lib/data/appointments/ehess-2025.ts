0c2b43e9888f942b7331b75ce7f4797850de08c9
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-05-16 22:33:31 +0200

    Update EHESS 2025 appointment details: Simplify institution name for clarity by removing extraneous information. This change enhances the readability of the appointment entry.

--- src/lib/data/appointments/ehess-2025.ts
+++ src/lib/data/appointments/ehess-2025.ts
@@ -3,7 +3,7 @@ import type { Appointment } from '$lib/types';
 export const ehess2025: Appointment = {
 	id: 'visiting-ehess-2025',
 	title: 'Visiting Scholar',
-	institution: 'Institut d’études de l’Islam et des sociétés du monde musulman, École des hautes études en sciences sociales (EHESS)',
+	institution: 'École des hautes études en sciences sociales (EHESS)',
 	location: 'Paris, France',
 	startYear: 2025,
 	endYear: 2025,
