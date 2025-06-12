77d9ffc9f2480462a43199a5f81c61402ba89e34
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-06-12 12:08:55 +0200

    refactor: Remove unused sr-only styles from MobileMenu component to streamline code and improve maintainability

--- src/lib/components/menu/MobileMenu.svelte
+++ src/lib/components/menu/MobileMenu.svelte
@@ -160,17 +160,7 @@
 		color: var(--color-primary);
 	}
 
-	.sr-only {
-		position: absolute;
-		width: 1px;
-		height: 1px;
-		padding: 0;
-		margin: -1px;
-		overflow: hidden;
-		clip: rect(0, 0, 0, 0);
-		white-space: nowrap;
-		border: 0;
-	}
+
 
 	:global(.mobile-nav-list) {
 		list-style: none;
