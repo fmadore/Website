27dbccd4260c8264e1f50c636f4b43e222677477
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-09-23 10:11:38 +0200

    feat: Add Publons record to African Affairs 2025 peer review entry

--- src/lib/data/peer-reviews/african-affairs-2025.ts
+++ src/lib/data/peer-reviews/african-affairs-2025.ts
@@ -6,7 +6,8 @@ export const africanAffairs2025: PeerReview = {
 	publisher: 'Oxford University Press',
 	type: 'Journal Article',
 	year: 2025,
-	dateISO: '2025-09-22'
+	dateISO: '2025-09-22',
+	publons_record: 'https://publons.com/wos-op/review/author/kl1iZ2Jl/'
 };
 
 export default africanAffairs2025;
