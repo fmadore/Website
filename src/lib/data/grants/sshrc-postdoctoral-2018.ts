f335286d480ea781d76bfa82fbf2e2fafeaba315
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-10-27 07:44:55 +0100

    feat: Add SSHRC Postdoctoral Research Award grant details

---
+++ src/lib/data/grants/sshrc-postdoctoral-2018.ts
@@ -0,0 +1,18 @@
+// src/lib/data/grants/sshrc-postdoctoral-2018.ts
+import type { Grant } from '$lib/types';
+
+export const sshrcPostdoctoral2018: Grant = {
+	id: 'sshrc-postdoctoral-2018',
+	title: 'Canada Postdoctoral Research Award',
+	funder: 'Social Sciences and Humanities Research Council of Canada (SSHRC)',
+	startYear: 2018,
+	endYear: 2020,
+	dateRangeString: '2018-2020',
+	dateISOStart: '2018-09-01',
+	dateISOEnd: '2020-08-31',
+	amount: 81000,
+	currency: 'CAD',
+	status: 'Turned Down'
+};
+
+export default sshrcPostdoctoral2018;
