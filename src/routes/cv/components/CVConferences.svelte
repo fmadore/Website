a1baf14f26c6c73dfb28e8cbef7fd77290c6f353
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-10-31 22:27:23 +0100

    fix: Enhance author formatting in conference participation section

--- src/routes/cv/components/CVConferences.svelte
+++ src/routes/cv/components/CVConferences.svelte
@@ -19,15 +19,18 @@
 			<div class="space-y-3">
 				{#each organizedPanels as comm (comm.id)}
 					{@const commDate = new Date(comm.dateISO)}
+					{@const formattedAuthors = formatCVAuthorList(comm.authors)}
 					<div class="flex gap-4">
 						<div class="font-semibold text-nowrap">{commDate.getFullYear()}</div>
 						<div class="flex-1">
-							"{comm.panelTitle || comm.title}".
-							{#if comm.conference}<em>{comm.conference}</em>{/if}{#if comm.location}, {comm.location}{/if}.
-							{commDate.toLocaleDateString('en-GB', {
-								day: 'numeric',
-								month: 'long'
-							})}.
+							{#if formattedAuthors}{@html formattedAuthors}. {/if}
+							"{comm.panelTitle || comm.title}"{#if comm.conference}, <em>{comm.conference}</em>{/if}{#if comm.location}, {comm.location}{/if}, {commDate.toLocaleDateString(
+								'en-GB',
+								{
+									day: 'numeric',
+									month: 'long'
+								}
+							)}.
 						</div>
 					</div>
 				{/each}
@@ -45,13 +48,13 @@
 						<div class="font-semibold text-nowrap">{commDate.getFullYear()}</div>
 						<div class="flex-1">
 							{#if formattedAuthors}{@html formattedAuthors}. {/if}
-							"{comm.title}".
-							{#if comm.panelTitle}Panel: <em>{comm.panelTitle}</em>. {/if}
-							{#if comm.conference}<em>{comm.conference}</em>{/if}{#if comm.location}, {comm.location}{/if}.
-							{commDate.toLocaleDateString('en-GB', {
-								day: 'numeric',
-								month: 'long'
-							})}.
+							"{comm.title}"{#if comm.panelTitle}, Panel: <em>{comm.panelTitle}</em>{/if}{#if comm.conference}, <em>{comm.conference}</em>{/if}{#if comm.location}, {comm.location}{/if}, {commDate.toLocaleDateString(
+								'en-GB',
+								{
+									day: 'numeric',
+									month: 'long'
+								}
+							)}.
 						</div>
 					</div>
 				{/each}
