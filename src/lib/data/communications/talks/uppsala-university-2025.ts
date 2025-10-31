f74d35d113217000d320c6a916560f4d012e6882
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-10-31 22:25:29 +0100

    feat: Add Uppsala University 2025 talk details to communications data

---
+++ src/lib/data/communications/talks/uppsala-university-2025.ts
@@ -0,0 +1,20 @@
+import type { Communication } from '$lib/types/communication';
+
+export const uppsalaUniversity2025: Communication = {
+	id: 'uppsala-university-2025',
+	title: 'TBA',
+	authors: ['Frédérick Madore'],
+	date: '5 December 2025',
+	dateISO: '2025-12-05',
+	year: 2025,
+	conference: 'Work talk ALMEDA project',
+	location: 'Uppsala University',
+	country: 'Sweden',
+	type: 'lecture',
+	language: 'English',
+	tags: ['IWAC', 'AI'],
+	coordinates: {
+		latitude: 59.8585638,
+		longitude: 17.6389267
+	}
+};
