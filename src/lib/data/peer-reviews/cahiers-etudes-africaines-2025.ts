45d40c494d2bedd0a2e89d0881ab9a16175f26be
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-11-07 10:58:01 +0100

    feat: Add new peer review entry for Cahiers d'études africaines 2025

---
+++ src/lib/data/peer-reviews/cahiers-etudes-africaines-2025.ts
@@ -0,0 +1,11 @@
+import type { PeerReview } from '$lib/types';
+
+export const cahiersEtudesAfricaines2025: PeerReview = {
+	id: 'cahiers-etudes-africaines-2025',
+	journal: 'Cahiers d\'études africaines',
+	type: 'Journal Article',
+	year: 2025,
+	dateISO: '2025-11-07'
+};
+
+export default cahiersEtudesAfricaines2025;
