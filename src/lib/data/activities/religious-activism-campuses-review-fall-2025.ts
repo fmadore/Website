3e794c23feefaca6f08bbdbb755a935073d572a8
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-01-14 11:48:48 +0100

    Update description and content fields

--- src/lib/data/activities/religious-activism-campuses-review-fall-2025.ts
+++ src/lib/data/activities/religious-activism-campuses-review-fall-2025.ts
@@ -8,9 +8,15 @@ export const activity: Activity = {
     date: formatDisplayDate('2026-01-14'),
     year: 2026,
     description:
-        "Honored that my book Religious Activism on Campuses in Togo and Benin has been reviewed by Macodou Fall in Research Africa Reviews.",
+        "Grateful that my book Religious Activism on Campuses in Togo and Benin has been reviewed by Macodou Fall in Research Africa Reviews.",
     content: `
-        <p>Honored that my book <a href="https://doi.org/10.1515/9783111428895" target="_blank" rel="noreferrer noopener"><em>Religious Activism on Campuses in Togo and Benin</em></a> has been reviewed by Macodou Fall in <a href="https://researchafrica.duke.edu/sites/default/files/-5-7-Religious-Activism-on-Campuses-in-Togo-and-Benin-Issue3-2025.pdf" target="_blank" rel="noreferrer noopener"><em>Research Africa Reviews</em></a>. Here's an excerpt:</p>
+        <p>Grateful that my book <a href="https://doi.org/10.1515/9783111428895" target="_blank" rel="noreferrer noopener"><em>Religious Activism on Campuses in Togo and Benin</em></a> has been reviewed by Macodou Fall in the latest issue of <a href="https://researchafrica.duke.edu/sites/default/files/-5-7-Religious-Activism-on-Campuses-in-Togo-and-Benin-Issue3-2025.pdf" target="_blank" rel="noreferrer noopener"><em>Research Africa Reviews</em></a>.</p>
+        
+        <p>I appreciate him taking the time to engage with the text. He offers thoughtful, constructive criticism regarding the "pivot from piety to pragmatism," suggesting that a deeper theological exploration of the "social curriculum" could enrich the analysis.</p>
+        
+        <p>He points out that the movements' economic survival could be considered a "moral and religious mandate for communal activism," rather than merely a response to state failure.</p>
+        
+        <p>Here's an excerpt:</p>
         
         <blockquote>
         <p>"These nuances, however, do not detract from the book's contribution, which is considerable; this is due to its comprehensive analysis of the history and development of faith-based movements in Togo and Benin from 1970 to 2023. The book offers a valuable resource for scholars seeking to understand the complex interplay between faith, activism, and adaptation in Francophone Africa."</p>
