9380504c6c194a359ef687a379f66d482022e7da
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-02-17 14:26:37 +0100

    Add new publication

---
+++ src/lib/data/publications/articles/madore-2025-iwac-private-database-public-resource.ts
@@ -0,0 +1,24 @@
+import type { Publication } from '$lib/types';
+
+export const madore2025IwacPrivateDatabasePublicResource: Publication = {
+	id: 'madore-2025-iwac-private-database-public-resource',
+	type: 'bulletin-article',
+	title:
+		'From Private Database to Public Resource to a Testing Ground for AI Methods: The Islam West Africa Collection (IWAC)',
+	authors: ['Frédérick Madore'],
+	date: '2025',
+	dateISO: '2025',
+	year: 2025,
+	journal: 'ZMO Jahrbuch 2023-2025',
+	publisher: 'Leibniz-Zentrum Moderner Orient',
+	publisherLocation: 'Germany',
+	language: 'English',
+	tags: ['Digital Humanities', 'IWAC', 'AI', 'West Africa', 'Archives', 'Open Access'],
+	url: 'https://www.zmo.de/fileadmin/user_upload/260209_ZMO_Triannual_2023_2025_webversion_reduziert.pdf',
+	image: 'images/publications/zmo-jahrbuch.webp',
+	heroImage: {
+		src: 'images/publications/zmo-jahrbuch-hero.webp',
+		alt: 'Cover of ZMO Jahrbuch'
+	},
+	project: 'Digital Humanities and AI in African Studies'
+};
