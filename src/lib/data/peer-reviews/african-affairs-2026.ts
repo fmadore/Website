6269b0172b8f47b6f596b0df92ed5179b87f323f
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-03-29 15:55:46 +0200

    Add African Affairs peer review (March 2026)

---
+++ src/lib/data/peer-reviews/african-affairs-2026.ts
@@ -0,0 +1,12 @@
+import type { PeerReview } from '$lib/types';
+
+export const africanAffairs2026: PeerReview = {
+	id: 'african-affairs-2026',
+	journal: 'African Affairs',
+	publisher: 'Oxford University Press',
+	type: 'Journal Article',
+	year: 2026,
+	dateISO: '2026-03-29'
+};
+
+export default africanAffairs2026;
